9beb243a39bc9af38e3e896b1f5756c3d8706d73
Kinara0 <kinarasoni@yahoo.com>
2021-11-08 21:48:15 -0500

    First Comment

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        //Circle,
+
+        Circle c = new Circle();
+        Circle circle1 = new Circle(2.0, 5.0, 9.0);
+
+        System.out.println(circle1);
+        double area = circle1.area();
+        System.out.println(area);
+        System.out.println(circle1.circumference());
+    }
+}
